1db305bbd838f69d163832b3cccb10638d6c7e8d
Pizza <fusionpizza00@gmail.com>
2019-12-06 05:45:53 -0800

    Create Visual Studio Project

---
+++ Source/QuestTemplate/Public/MyMotionControllerComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MotionControllerComponent.h"
+#include "MyMotionControllerComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class QUESTTEMPLATE_API UMyMotionControllerComponent : public UMotionControllerComponent
+{
+	GENERATED_BODY()
+	
+};
